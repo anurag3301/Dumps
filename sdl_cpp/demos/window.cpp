de5ba49634ae9e0ad801e837da5579a0712ae29d
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2022-11-25 21:05:56 +0530

    Added code for create a simple window

--- sdl_cpp/demos/window.cpp
+++ sdl_cpp/demos/window.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-#include <SDL/SDL.h>
+#include <SDL2/SDL.h>
 
 using std::cout;
+using std::cerr;
 using std::endl;
 
 int main(){
@@ -10,7 +11,26 @@ int main(){
         return 1;
     }
 
-    cout << "SDL Init succeeded..." << endl;
+    SDL_Window *window = SDL_CreateWindow("Particle Fire Explosion", 
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 500, 500, 0);
+
+    if(window == NULL){
+        cerr << "SDL window failed to initialise: " << SDL_GetError() << endl;
+        SDL_Quit();
+        return 2;
+    }
+
+    bool quit = false;
+    SDL_Event event;
+    while(!quit){
+        while(SDL_PollEvent(&event)){
+            if(event.type == SDL_QUIT){
+                quit = true;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
