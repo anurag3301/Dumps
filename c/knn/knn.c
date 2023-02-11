088eba7333dee146460cd2d3b3fa32495510279d
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2023-02-11 23:12:27 +0530

    Got the structure of csv reader

--- c/knn/knn.c
+++ c/knn/knn.c
@@ -2,24 +2,40 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main(){
-    FILE *f = fopen("iris.csv", "r");
-    char *line_buf = NULL;
+
+typedef struct Column{
+    char* colname;
+    char** colvals;
+    size_t col_count;
+}Column;
+
+typedef struct Table{
+    Column *cols;
+    size_t row_count, col_count;
+    char** col_names;
+}Table;
+
+Table* init_table(char* file_name){
+    FILE *f = fopen(file_name, "r");
+    char *line = NULL;
     size_t len = 0;
-
-
 
     if(f == NULL){
         printf("Error in reading file");
-        return 1;
+        return NULL;
     }
 
-    getline(&line_buf, &len, f);
+    Table* table = malloc(sizeof(table));
 
-    rewind(f);
+    getline(&line, &len, f);
+    uint comma_numer = 0;
+    for(int i=0; line[i]!='\0'; i++)if(line[i]==',')comma_numer++;
+    printf("%d", comma_numer);
+    
+    free(line);
+    return table;
+}
 
-    getline(&line_buf, &len, f);
-
-    fclose(f);
-    free(line_buf);
+int main(){
+    Table* table = init_table("iris.csv");
 }
