a87b2d7be5eb9ab3935a126f95d68789154050ea
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2023-02-12 22:31:07 +0530

    Createion and initialisation of columns done

--- c/knn/knn.c
+++ c/knn/knn.c
@@ -6,7 +6,7 @@
 typedef struct Column{
     char* colname;
     char** colvals;
-    size_t col_count;
+    size_t row_count;
 }Column;
 
 typedef struct Table{
@@ -53,22 +53,39 @@ Table* init_table(char* file_name){
     size_t comma_count = 0;
     for(int i=0; line[i]!='\0'; i++)if(line[i]==',')comma_count++;
     table->col_count = ++comma_count;
+    table->cols = malloc(sizeof(Column) * table->col_count);
 
     rewind(f);
     size_t line_count = 0;
     while(getline(&line, &len, f) != -1)line_count++;
     table->row_count = line_count;
+    for(size_t i=0; i<table->col_count; i++){
+        table->cols[i].colvals = malloc(sizeof(char*)*table->row_count);
+        table->cols[i].row_count = table->row_count;
+    }
     
     rewind(f);
     getline(&line, &len, f);
     char** col_names = parse_row(line, table->col_count);
     table->col_names = col_names;
     for(int i=0; i<table->col_count; i++){
-        printf("%s\n", table->col_names[i]);
+        table->cols[i].colname = table->col_names[i];
     }
 
     free(line);
+    fclose(f);
     return table;
+}
+
+void distroy_table(Table* table){
+    for(size_t i=0; i<table->col_count; i++){
+        free(table->col_names[i]);
+        free(table->cols[i].colvals);
+    }
+    free(table->col_names);
+    free(table->cols);
+
+    free(table);
 }
 
 
@@ -76,5 +93,5 @@ Table* init_table(char* file_name){
 int main(){
     Table* table = init_table("iris.csv");
     /* char** res = parse_row("hel,my,name,is,asdf", 5); */
-    free(table);
+    distroy_table(table);
 }
