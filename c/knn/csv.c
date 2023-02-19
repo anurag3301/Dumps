6d3e4649938fd53bf3e825ea77ee2254a38fe62c
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2023-02-19 09:48:25 +0530

    Fixed CSV row parsing function, missing last character

--- c/knn/csv.c
+++ c/knn/csv.c
@@ -19,7 +19,7 @@ char** parse_row(char* line, size_t col_num){
     int len = strlen(line);
     for(int pos=0, idx=0; pos < len; pos++, idx++){
         int i;
-        for(i=pos; line[i]!='\0'&&line[i]!=','; i++); 
+        for(i=pos; line[i]!='\0'&&line[i]!=','&&line[i]!='\n'; i++); 
         str_vec[idx] = substr(line, pos, i-pos);
         pos = i;
     }
@@ -56,7 +56,6 @@ Table* init_table(char* file_name){
     
     rewind(f);
     getline(&line, &len, f);
-    line[strlen(line)-2] = '\0';
     char** col_names = parse_row(line, table->col_count);
     table->col_names = col_names;
     for(int i=0; i<table->col_count; i++){
@@ -67,7 +66,6 @@ Table* init_table(char* file_name){
     rewind(f);
     getline(&line, &len, f);
     for(size_t line_no=0; getline(&line, &len, f) != -1; line_no++){
-        line[strlen(line)-2] = '\0';
         char** res = parse_row(line, table->col_count);
         for(size_t i=0; i<table->col_count; i++){
             table->cols[i].colvals[line_no] = res[i];
@@ -141,12 +139,12 @@ void distroy_row(Row* row){
 
 void print_table(Table* table){
     for(size_t i=0; i<table->col_count; i++){
-        printf("%s\t", table->col_names[i]);
+        printf("%s,\t", table->col_names[i]);
     }
     for(size_t row=0; row<table->row_count; row++){
         printf("\n");
         for(size_t col=0; col<table->col_count; col++){
-            printf("%s\t", table->cols[col].colvals[row]);
+            printf("%s,\t", table->cols[col].colvals[row]);
         }
     }
     printf("\n");
