a89f093ff4d3d85b94e99adc544f81a1b648d72a
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2023-03-05 13:32:51 +0530

    Added Creation and destruction of matrix

--- c/game_of_life/main.c
+++ c/game_of_life/main.c
@@ -5,6 +5,24 @@
 WINDOW *create_newwin(int height, int width, int starty, int startx);
 void destroy_win(WINDOW *local_win);
 
+char** create_matrix(int size, int prob){
+    char** matrix = malloc(sizeof(char*) * size);
+    for(size_t i=0; i<size; i++){
+        matrix[i] = malloc(sizeof(char) * size);
+        for(size_t j=0; j<size; j++){
+            matrix[i][j] = (rand() % 100) < prob;
+        }
+    }
+
+    return matrix;
+}
+
+void destroy_matrix(char** mat, int size){
+    for(size_t i=0; i<size; i++){
+        free(mat[i]);
+    }
+    free(mat);
+}
 int main(int argc, char *argv[]){	
     WINDOW *my_win;
 	int startx, starty, width, height;
@@ -18,10 +36,15 @@ int main(int argc, char *argv[]){
 	width = height*2;
 	starty = (LINES - height) / 2;
 	startx = (COLS - width) / 2;
-	refresh();
 
+
+	refresh();
     my_win = create_newwin(height, width, starty, startx);
-		
+
+    char** matrix = create_matrix(height, 100);
+    destroy_matrix(matrix, height);
+
+
     getch();
 	endwin();
 	return 0;
