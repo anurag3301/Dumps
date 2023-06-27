9a4481276ceb1ae65c0156e4468fae1e984fd11a
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2023-06-27 19:55:36 +0530

    ------

--- sdl_cpp/demos/window.cpp
+++ sdl_cpp/demos/window.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#define HEIGHT 500
+#define WIDTH 500
 
 using std::cout;
 using std::cerr;
@@ -12,7 +14,7 @@ int main(){
     }
 
     SDL_Window *window = SDL_CreateWindow("Particle Fire Explosion", 
-            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 500, 500, 0);
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, 0);
 
     if(window == NULL){
         cerr << "SDL window failed to initialise: " << SDL_GetError() << endl;
@@ -20,6 +22,12 @@ int main(){
         return 2;
     }
 
+
+    SDL_Renderer *render = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+    SDL_Texture *texture = SDL_CreateTexture(render, SDL_PIXELFORMAT_RGB888,
+            SDL_TEXTUREACCESS_STATIC, WIDTH, HEIGHT);
+
+    Uint32 *buffer = new Uint32[WIDTH * HEIGHT];
     bool quit = false;
     SDL_Event event;
     while(!quit){
@@ -30,6 +38,9 @@ int main(){
         }
     }
 
+    delete []buffer;
+    SDL_DestroyRenderer(render);
+    SDL_DestroyTexture(texture);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
